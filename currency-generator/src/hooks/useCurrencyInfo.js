ebfb98695a7590bfc83aa964aaff607d4209f6f9
Divyanshi Rastogi <100357804+udayramisetty@users.noreply.github.com>
2025-07-26 23:11:54 +0530

    creating-the-input-logic

--- currency-generator/src/hooks/useCurrencyInfo.js
+++ currency-generator/src/hooks/useCurrencyInfo.js
@@ -8,13 +8,13 @@ function useCurrencyInfo(currency) {
     //so which hook we can invoke when we want to mount or unmount the component. when the component is mount its lifestyle event is triggered 
     useEffect(() => {
         //call api and convert output from this url is in string format and convert it to json 
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-            .then((res) => res.json)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then((res) => res.json())
             .then((res) => setData(res[currency]))
         console.log(data);
     }, [currency])
     console.log(data);
-    return data;
+    return data
 }
 
 export default useCurrencyInfo;
