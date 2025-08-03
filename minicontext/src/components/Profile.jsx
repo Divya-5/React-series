3b26eb7b579a8165bdeb4e1f4512c9c891e0eba5
Divyanshi Rastogi <100357804+udayramisetty@users.noreply.github.com>
2025-08-03 19:29:58 +0530

    context-api-part-1

---
+++ minicontext/src/components/Profile.jsx
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+import UserContext from "../../context/UserContext";
+
+export const Profile = () => {
+  const { user } = useContext(UserContext);
+  if (!user) return <div>Please Login</div>;
+  return <div>Welcome {user.username}</div>;
+};
